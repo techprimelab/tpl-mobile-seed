3a1ce79385fdf68a203e322baccda356d9d69509
roshankarwalkar1 <roshan.karwalkar@techprimelab.com>
2016-06-16 11:28:46 +0530

    Added jquery typescript so we can use it as object,tried simple two way data binding.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,13 @@
+/// <reference path="../node_modules/typescript/lib/jquery.d.ts"/>
 import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<h1>My First angular  2 App{{student.name}}</h1>'
+
 })
-export class AppComponent { }
+export class AppComponent {
+student={
+    name:"roshan",
+    height:56
+};
+}
